7ff93d3c47df01afccfcd93d154f5db12b7ceb5d
Ivanduik <ivictorv1982@gmail.com>
2023-04-22 14:19:55 +0300

    Hw1 2t corrected2

--- src/hw1/t3/Car.java
+++ src/hw1/t3/Car.java
@@ -20,13 +20,11 @@ public class Car implements Comparable<Car> {
 
     @Override
     public int compareTo(Car o) {
-        int temp = this.speed - o.speed;   // int temp = this.speed - o.speed;
+        int temp = this.speed - o.speed;
         if (temp == 0) {
             if ((this.price - o.price) == 0) {
                 if (this.model.compareTo(o.model) == 0) {
-                    if (this.color.compareTo(o.color) != 0) {
-                        return this.color.compareTo(o.color);
-                    } else return temp;
+                    return this.color.compareTo(o.color);
                 } else return this.model.compareTo(o.model);
             } else return this.price - o.price;
         } else {
