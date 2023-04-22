a786cfe748c1c9c7a80381a24f912c2ffaab974c
Ivanduik <ivictorv1982@gmail.com>
2023-04-22 18:00:31 +0300

    Hw1 2t corrected2.1

--- src/hw1/t3/Main.java
+++ src/hw1/t3/Main.java
@@ -15,6 +15,11 @@ public class Main {
         Car c9 = new Car(190, 2000, "Opel", "Black");
         Car c10 = new Car(185, 10000, "Mercedes", "White");
         Car[] c = {c1, c2, c3, c4, c5,c6,c7,c8,c9,c10};
+
+        for (int i=0; i<c.length;i++){
+            for (int j=i+1;j<c.length-1;j++)
+                System.out.println(c[i]+" compareTo "+c[j]+" = "+c[i].compareTo(c[j]));
+        }
         Arrays.sort(c);
         for (Car temp : c) {
             System.out.println(temp);
